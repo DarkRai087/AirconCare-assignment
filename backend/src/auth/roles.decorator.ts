69f876a4d32aa09bf55ff50e2ae50b922ae54a2e
DarkRai087 <parthp90111@gmail.com>
2025-06-15 18:09:45 +0530

    coms

--- backend/src/auth/roles.decorator.ts
+++ backend/src/auth/roles.decorator.ts
@@ -1,3 +1,3 @@
 import { SetMetadata } from '@nestjs/common';
-
+//Checks if the authenticated user has the required role for a route.
      export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
